6355a90161cb9c2b43ce2218081dfc4e05172749
JKooner1 <jkoone300@caledonian.ac.uk>
2024-11-08 01:21:14 +0000

    Updating TestRunner to Fix sonar

--- TestRunner.java
+++ TestRunner.java
@@ -1,22 +1,27 @@
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class TestRunner {
+    // Set up a logger for the TestRunner class
+    private static final Logger logger = Logger.getLogger(TestRunner.class.getName());
+
     public static void main(String[] args) {
-        // Specify the test classes you want to run
+        // Run the tests in Dec2HexTest
         Result result = JUnitCore.runClasses(Dec2HexTest.class);
 
-        // Display results
+        // Display results for any failed tests
         for (Failure failure : result.getFailures()) {
-            System.out.println(failure.toString());
+            logger.log(Level.SEVERE, failure.toString());
         }
 
         // Display if the tests were successful
         if (result.wasSuccessful()) {
-            System.out.println("All tests passed successfully.");
+            logger.log(Level.INFO, "All tests passed successfully.");
         } else {
-            System.out.println("Some tests failed.");
+            logger.log(Level.WARNING, "Some tests failed.");
         }
     }
 }
